a80cd4b97de9cda52acb3a4ed60222afff4aa7e2
0xHampton <hampton.j.black@gmail.com>
2023-11-17 23:16:14 -0500

    Add ERC1155Pausable, ERC1155Burnable, and ERC1155Supply to RealWorldAsset contract

--- packages/foundry/contracts/RealWorldAsset.sol
+++ packages/foundry/contracts/RealWorldAsset.sol
@@ -3,26 +3,53 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
 
-contract RealWorldAsset is ERC1155, AccessControl {
+contract RealWorldAsset is ERC1155, AccessControl, ERC1155Pausable, ERC1155Burnable, ERC1155Supply {
+    bytes32 public constant URI_SETTER_ROLE = keccak256("URI_SETTER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
-    constructor(address deployer) ERC1155("https://example.com/token/{id}.json") {
+    constructor(address deployer) ERC1155("") {
         _grantRole(DEFAULT_ADMIN_ROLE, deployer);
         _grantRole(MINTER_ROLE, deployer);
+        _grantRole(PAUSER_ROLE, deployer);
     }
 
-    function mint(address account, uint256 id, uint256 amount, bytes memory data) public {
-        require(hasRole(MINTER_ROLE, msg.sender), "RealWorldAsset: must have minter role to mint");
+    function mint(address account, uint256 id, uint256 amount, bytes memory data) public onlyRole(MINTER_ROLE) {
         _mint(account, id, amount, data);
     }
 
-    function burn(address account, uint256 id, uint256 amount) public {
-        require(
-            account == msg.sender || hasRole(DEFAULT_ADMIN_ROLE, msg.sender),
-            "RealWorldAsset: must have admin role or be the account owner to burn"
-        );
-        _burn(account, id, amount);
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyRole(MINTER_ROLE)
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    function setURI(string memory newuri) public onlyRole(URI_SETTER_ROLE) {
+        _setURI(newuri);
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    // *********************************************************************************************
+    // * The following functions are overrides required by Solidity.
+    // *********************************************************************************************
+
+    function _update(address from, address to, uint256[] memory ids, uint256[] memory values)
+        internal
+        override(ERC1155, ERC1155Pausable, ERC1155Supply)
+    {
+        super._update(from, to, ids, values);
     }
 
     function supportsInterface(bytes4 interfaceId)
