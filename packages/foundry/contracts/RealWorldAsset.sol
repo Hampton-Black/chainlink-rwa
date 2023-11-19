aaa98d64d91d080f8e305a511a509fda82bc64f1
0xHampton <hampton.j.black@gmail.com>
2023-11-19 15:49:52 -0500

    Add cannotExecute modifier to checkUpkeep()

--- packages/foundry/contracts/RealWorldAsset.sol
+++ packages/foundry/contracts/RealWorldAsset.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {AccessControl, IAccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 import {ERC1155Pausable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Pausable.sol";
 import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
@@ -101,6 +101,7 @@ contract RealWorldAsset is
         external
         view
         override
+        cannotExecute
         returns (bool upkeepNeeded, bytes memory performData)
     {
         if (upkeepRequested || (block.timestamp - lastTimeStamp) < interval) {
