fe8d3170f7b902ac868509711617e0601f4d0a76
0xHampton <hampton.j.black@gmail.com>
2023-11-19 17:21:15 -0500

    Add script to generate encoded arguments

---
+++ packages/foundry/script/generateEncodedArgs.js
@@ -0,0 +1,12 @@
+const ethers = require("ethers");
+
+// use ethers to test out abi.encoding of input parameters
+const abi = new ethers.utils.AbiCoder();
+const encoded = abi.encode(
+  ["string", "string", "string"],
+  ["test", "real estate", "florida"]
+);
+
+// const encodedID = abi.encode(["uint256"], [1]);
+
+console.log(encoded);
