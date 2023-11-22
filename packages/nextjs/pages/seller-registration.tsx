b23e3f412bef4adc30fa0bc355f9617bd4d1521e
0xHampton <hampton.j.black@gmail.com>
2023-11-22 16:35:45 -0500

    Refactor seller registration page components

--- packages/nextjs/pages/seller-registration.tsx
+++ packages/nextjs/pages/seller-registration.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
-import { AddressInput } from "~~/components/scaffold-eth";
+import { AddressInputField } from "~~/components/AddressInputField";
+import { FormInput } from "~~/components/FormInput";
+import { StepList } from "~~/components/StepList";
 
 const SellerRegistration: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -26,9 +28,22 @@ const SellerRegistration: React.FC = () => {
     });
   };
 
+  const handleAddressChange = (newValue: string) => {
+    handleChange({
+      target: {
+        name: "walletAddress",
+        value: newValue,
+      },
+    } as React.ChangeEvent<HTMLInputElement>);
+  };
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     // Handle form submission logic here
+  };
+
+  const onStepClick = (stepNumber: number) => {
+    setCurrentPage(stepNumber);
   };
 
   // Update the walletAddress field when the user connects their wallet
@@ -46,12 +61,7 @@ const SellerRegistration: React.FC = () => {
         </div>
 
         <div className="col-span-1 ml-48 p-4">
-          <ul className="steps steps-vertical">
-            <li className={currentPage >= 1 ? "step step-primary" : "step"}>Register with PolygonID</li>
-            <li className={currentPage >= 2 ? "step step-primary" : "step"}>Choose Asset Type</li>
-            <li className={currentPage >= 3 ? "step step-primary" : "step"}>Provide Additional Details</li>
-            <li className={currentPage >= 4 ? "step step-primary" : "step"}>Review and Submit</li>
-          </ul>
+          <StepList currentPage={currentPage} onStepClick={onStepClick} />
         </div>
 
         <div className="col-span-1 flex items-center justify-center">
@@ -60,43 +70,29 @@ const SellerRegistration: React.FC = () => {
           {currentPage === 1 && (
             <div className="form-control w-full max-w-lg p-4">
               <form onSubmit={nextPage} className="flex flex-col justify-around h-full">
-                <label className="label justify-between">
-                  <span className="label-text" style={{ width: "100px" }}>
-                    First Name:
-                  </span>
-                  <input
-                    type="text"
-                    placeholder="First Name"
-                    name="firstName"
-                    value={formData.firstName}
-                    onChange={handleChange}
-                    className="input w-full"
-                  />
-                </label>
+                <FormInput
+                  label="First Name:"
+                  placeholder="First Name"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                />
 
-                <label className="label">
-                  <span className="label-text" style={{ width: "100px" }}>
-                    Last Name:
-                  </span>
-                  <input
-                    type="text"
-                    placeholder="Last Name"
-                    name="lastName"
-                    value={formData.lastName}
-                    onChange={handleChange}
-                    className="input w-full"
-                  />
-                </label>
+                <FormInput
+                  label="Last Name:"
+                  placeholder="Last Name"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                />
 
-                <label className="label">
-                  <span className="label-text">Wallet Address:</span>
-                  <AddressInput
-                    onChange={(newValue: string) => setFormData({ ...formData, walletAddress: newValue })}
-                    value={formData.walletAddress}
-                    placeholder="Wallet Address"
-                    name="walletAddress"
-                  />
-                </label>
+                <AddressInputField
+                  label="Wallet Address:"
+                  placeholder="Wallet Address"
+                  name="walletAddress"
+                  value={formData.walletAddress}
+                  onChange={handleAddressChange}
+                />
                 <div className="self-end p-4">
                   <button type="submit" className="btn btn-sm btn-outline">
                     Next
