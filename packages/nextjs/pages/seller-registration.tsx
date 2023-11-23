da96e020a6c9fea45e58adcd10ab1fd1432787ad
0xHampton <hampton.j.black@gmail.com>
2023-11-23 09:16:07 -0500

    - Refactor seller registration form - Add image upload functionality - Add useEffect hook to fetch api Data

--- packages/nextjs/pages/seller-registration.tsx
+++ packages/nextjs/pages/seller-registration.tsx
@@ -1,22 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { AssetTypeSelection } from "./../components/AssetTypeSelection";
+import axios from "axios";
 import { useAccount } from "wagmi";
 import { AddressInputField } from "~~/components/AddressInputField";
 import { FormInput } from "~~/components/FormInput";
 import { StepList } from "~~/components/StepList";
 
+interface FormData {
+  firstName: string;
+  lastName: string;
+  walletAddress: string;
+  assetType: string;
+  assetName: string;
+  assetLocation: string;
+  uploadedImage: File | null;
+}
+
 const SellerRegistration: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<FormData>({
     firstName: "",
     lastName: "",
     walletAddress: "",
     assetType: "",
     assetName: "",
     assetLocation: "",
+    uploadedImage: null,
   });
   const [selectedOption, setSelectedOption] = useState("");
   const [useApi, setUseApi] = useState<boolean | undefined>(undefined);
+  const [apiData, setApiData] = useState(null);
+  const [uploadedImage, setUploadedImage] = useState<File | null>(null);
   const accountState = useAccount();
 
   const optionMapping = {
@@ -61,9 +75,28 @@ const SellerRegistration: React.FC = () => {
     } as React.ChangeEvent<HTMLInputElement>);
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleFinalSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     // Handle form submission logic here
+  };
+
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      setUploadedImage(event.target.files[0]);
+    }
+  };
+
+  const handleImageSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    setFormData({
+      ...formData,
+      uploadedImage,
+    });
+
+    // Handle form submission with updatedFormData here
+
+    nextPage();
   };
 
   const onStepClick = (stepNumber: number) => {
@@ -76,6 +109,35 @@ const SellerRegistration: React.FC = () => {
       setFormData(prevFormData => ({ ...prevFormData, walletAddress: accountState.address ?? "" }));
     }
   }, [accountState.address]);
+
+  // Fetch data from the API when useApi changes
+  useEffect(() => {
+    if (useApi) {
+      // Split the full address into parts around comma (i.e. "4525 Dean Martin Dr, Las Vegas, NV 12434")
+      const [street, rest] = formData.assetLocation.split(",").map(part => part.trim());
+      // Split the rest of the address into parts around the last space (i.e. "Las Vegas, NV 12434"), discarding the last part (i.e. "12434")
+      const [cityState] = rest.split(/\s(?=\d+$)/);
+
+      const address1 = street;
+      const address2 = cityState;
+
+      axios
+        .get("https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/basicprofile", {
+          headers: {
+            accept: "application/json",
+            apikey: process.env.NEXT_PUBLIC_ATTOM_API_KEY,
+            "accept-language": "en",
+            "Content-Type": "application/json",
+          },
+          params: {
+            address1: address1,
+            address2: address2,
+          },
+        })
+        .then(response => setApiData(response.data))
+        .catch(error => console.error("Error:", error));
+    }
+  }, [useApi, formData.assetLocation]);
 
   return (
     <div>
@@ -151,7 +213,7 @@ const SellerRegistration: React.FC = () => {
           {currentPage === 3 && (
             <div className="form-control w-full max-w-lg p-4">
               <h2 className="text-2xl text-center p-4">Required Data to mint your RWA NFT</h2>
-              <form onSubmit={nextPage} className="flex flex-col justify-around h-full">
+              <form onSubmit={handleImageSubmit} className="flex flex-col justify-around h-full">
                 <FormInput
                   label="Asset Name:"
                   placeholder="Name of Asset"
@@ -174,7 +236,7 @@ const SellerRegistration: React.FC = () => {
                   </label>
                   <input
                     type="file"
-                    placeholder="Image of Asset"
+                    onChange={handleImageUpload}
                     className="file-input file-input-bordered file-input-accent w-full"
                   />
                 </div>
@@ -212,9 +274,19 @@ const SellerRegistration: React.FC = () => {
                     </button>
                   </div>
                 ) : useApi ? (
-                  <div>Render fetched data from API here</div>
+                  apiData ? (
+                    <div className="mockup-code">
+                      <pre>
+                        <code>{JSON.stringify(apiData, null, 2)}</code>
+                      </pre>
+                    </div>
+                  ) : (
+                    <div className="flex justify-center items-center h-full">
+                      <span className="loading loading-dots loading-md"></span>
+                    </div>
+                  )
                 ) : (
-                  <div>Render form inputs here</div>
+                  <div>Render Manual form inputs here</div>
                 )}
 
                 <div className="flex justify-between pt-4">
@@ -232,7 +304,7 @@ const SellerRegistration: React.FC = () => {
           {currentPage === 6 && (
             <div className="form-control w-full max-w-lg p-4">
               <h2 className="text-2xl text-center p-4">Review and Submit</h2>
-              <form onSubmit={handleSubmit}>
+              <form onSubmit={handleFinalSubmit}>
                 <div className="mockup-code">
                   <pre>
                     <code>
