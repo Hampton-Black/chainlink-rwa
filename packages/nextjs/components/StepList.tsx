8ce6ab0d6aaf2770a14c6276031a44a3f1eb549c
0xHampton <hampton.j.black@gmail.com>
2023-11-22 22:38:00 -0500

    Update step names in StepList component

--- packages/nextjs/components/StepList.tsx
+++ packages/nextjs/components/StepList.tsx
@@ -14,10 +14,16 @@ export const StepList: React.FC<StepListProps> = ({ currentPage, onStepClick }) 
       <button onClick={() => onStepClick(2)}>Choose Asset Type</button>
     </li>
     <li className={currentPage >= 3 ? "step step-primary" : "step"}>
-      <button onClick={() => onStepClick(3)}>Provide Additional Details</button>
+      <button onClick={() => onStepClick(3)}>Required Data for NFT</button>
     </li>
     <li className={currentPage >= 4 ? "step step-primary" : "step"}>
-      <button onClick={() => onStepClick(4)}>Review and Submit</button>
+      <button onClick={() => onStepClick(4)}>Additional Details for Metadata</button>
+    </li>
+    <li className={currentPage >= 5 ? "step step-primary" : "step"}>
+      <button onClick={() => onStepClick(5)}>Legal Contract</button>
+    </li>
+    <li className={currentPage >= 6 ? "step step-primary" : "step"}>
+      <button onClick={() => onStepClick(6)}>Review and Submit</button>
     </li>
   </ul>
 );
