19747c284441d96e2f645b607e5e4e372e62d055
0xHampton <hampton.j.black@gmail.com>
2023-11-26 16:17:26 -0500

    Refactor ApiDataDisplay component to use flex layout and add JSON view

--- packages/nextjs/components/ApiDataDisplay.tsx
+++ packages/nextjs/components/ApiDataDisplay.tsx
@@ -1,5 +1,5 @@
 export const ApiDataDisplay: React.FC<{ apiData: any }> = ({ apiData }) => (
-  <div className="mockup-code">
+  <div className="flex container w-full overflow-auto h-96 json-view rounded-lg p-4">
     <pre>
       <code>{JSON.stringify(apiData, null, 2)}</code>
     </pre>
