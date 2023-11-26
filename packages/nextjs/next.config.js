47391a760c26caf9d25940cd1cf968a5bf490a77
0xHampton <hampton.j.black@gmail.com>
2023-11-26 16:16:23 -0500

    Add @svgr/webpack to devDependencies

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -10,6 +10,28 @@ const nextConfig = {
     ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
   webpack: config => {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find(rule => rule.test?.test?.(".svg"));
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        // resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      },
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
   },
