785f485fcdd4febf08b21e8c19e05ff20eea5af7
0xHampton <hampton.j.black@gmail.com>
2023-11-26 17:07:53 -0500

    Add margin-left to ApiDataDisplay component

--- packages/nextjs/components/ApiDataDisplay.tsx
+++ packages/nextjs/components/ApiDataDisplay.tsx
@@ -1,5 +1,5 @@
 export const ApiDataDisplay: React.FC<{ apiData: any }> = ({ apiData }) => (
-  <div className="flex container w-full overflow-auto h-96 json-view rounded-lg p-4">
+  <div className="flex container w-full overflow-auto h-96 json-view rounded-lg p-4 ml-36">
     <pre>
       <code>{JSON.stringify(apiData, null, 2)}</code>
     </pre>
