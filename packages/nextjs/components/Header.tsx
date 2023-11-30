c579b86b1b2552f022785e2924c041bfdf72629c
@hampton.black <hampton.black@mattereum.com>
2023-11-30 15:20:42 -0500

    Add "Create" link to header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { PlusIcon } from "@heroicons/react/20/solid";
 import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
@@ -16,6 +17,11 @@ export const menuLinks: HeaderMenuLink[] = [
   {
     label: "Home",
     href: "/",
+  },
+  {
+    label: "Create",
+    href: "/create",
+    icon: <PlusIcon className="h-4 w-4" />,
   },
   {
     label: "Debug Contracts",
