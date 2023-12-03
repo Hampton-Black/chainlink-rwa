6adff5fc76c756166805dc9b39866138a480e846
0xHampton <hampton.j.black@gmail.com>
2023-12-03 13:02:44 -0500

    Update step names in StepList component

--- packages/nextjs/components/StepList.tsx
+++ packages/nextjs/components/StepList.tsx
@@ -12,22 +12,19 @@ export const StepList: React.FC<StepListProps> = ({ currentPage, onStepClick }) 
   return (
     <ul className="steps steps-vertical">
       <li className={currentPage >= 1 ? `step ${isDarkTheme ? "step-accent" : "step-primary"}` : "step"}>
-        <button onClick={() => onStepClick(1)}>Register with PolygonID</button>
+        <button onClick={() => onStepClick(1)}>Choose Asset Type</button>
       </li>
       <li className={currentPage >= 2 ? `step ${isDarkTheme ? "step-accent" : "step-primary"}` : "step"}>
-        <button onClick={() => onStepClick(2)}>Choose Asset Type</button>
+        <button onClick={() => onStepClick(2)}>Required Data</button>
       </li>
       <li className={currentPage >= 3 ? `step ${isDarkTheme ? "step-accent" : "step-primary"}` : "step"}>
-        <button onClick={() => onStepClick(3)}>Required Data for NFT</button>
+        <button onClick={() => onStepClick(3)}>Additional Details</button>
       </li>
       <li className={currentPage >= 4 ? `step ${isDarkTheme ? "step-accent" : "step-primary"}` : "step"}>
-        <button onClick={() => onStepClick(4)}>Additional Details for Metadata</button>
+        <button onClick={() => onStepClick(4)}>Legal Contract</button>
       </li>
       <li className={currentPage >= 5 ? `step ${isDarkTheme ? "step-accent" : "step-primary"}` : "step"}>
-        <button onClick={() => onStepClick(5)}>Legal Contract</button>
-      </li>
-      <li className={currentPage >= 6 ? `step ${isDarkTheme ? "step-accent" : "step-primary"}` : "step"}>
-        <button onClick={() => onStepClick(6)}>Review and Submit</button>
+        <button onClick={() => onStepClick(5)}>Review and Submit</button>
       </li>
     </ul>
   );
