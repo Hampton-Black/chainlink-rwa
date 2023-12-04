40a0d2c8347a4ac69eea36331752d9694867e0ab
0xHampton <hampton.j.black@gmail.com>
2023-12-04 14:08:44 -0500

    Add modal for final steps in create page

--- packages/nextjs/pages/create.tsx
+++ packages/nextjs/pages/create.tsx
@@ -1,10 +1,18 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
+import { ethers } from "ethers";
 import { NextPage } from "next";
+import { renderToStaticMarkup } from "react-dom/server";
+import { useAccount } from "wagmi";
 import { ApiDataDisplay } from "~~/components/ApiDataDisplay";
 import BaseThumbnail from "~~/components/BaseThumbnail";
+import { FormButtons } from "~~/components/FormButtons";
+import LegalContract from "~~/components/LegalContract";
 import { ManualFormInputs } from "~~/components/ManualFormInputs";
 import { EtherInput } from "~~/components/scaffold-eth";
+import { ThemeContext } from "~~/contexts/ThemeContext";
+import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
+import { pinFileToIPFS, pinJSONToIPFS } from "~~/services/pinataService";
 
 interface FormState {
   title: string;
@@ -16,7 +24,14 @@ interface FormState {
   expiryDate: Date;
 }
 
+interface LegalContractData {
+  signature: string;
+  contractURI: string;
+}
+
 const MattereumPassport: NextPage = () => {
+  const { isDarkTheme } = useContext(ThemeContext);
+
   const [formData, setFormData] = useState<FormState>({
     title: "",
     category: "",
@@ -29,6 +44,11 @@ const MattereumPassport: NextPage = () => {
   const [useApi, setUseApi] = useState<boolean | undefined>(undefined);
   const [apiData, setApiData] = useState(null);
   const [manualFields, setManualFields] = useState([{ key: "", value: "" }]);
+  const [fullNFTImageURI, setFullNFTImageURI] = useState("");
+  const [fullMetadataURI, setFullMetadataURI] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentModalPage, setCurrentModalPage] = useState(1);
+  const [legalContractData, setLegalContractData] = useState<LegalContractData | null>(null);
 
   const assetCategories = [
     "Real Estate",
@@ -40,6 +60,35 @@ const MattereumPassport: NextPage = () => {
     "Vehicles",
     "Other",
   ];
+
+  const accountState = useAccount();
+
+  // write to mint function
+  const { writeAsync: writeAsyncMint } = useScaffoldContractWrite({
+    contractName: "RealWorldAsset",
+    functionName: "mint",
+    args: [undefined, undefined, undefined],
+    // For payable functions
+    value: undefined,
+    // The number of block confirmations to wait for before considering transaction to be confirmed (default : 1).
+    blockConfirmations: 1,
+    // The callback function to execute when the transaction is confirmed.
+    onBlockConfirmation: txnReceipt => {
+      console.log("Transaction blockHash", txnReceipt.blockHash);
+    },
+  });
+
+  const nextPage = () => {
+    setCurrentModalPage(currentModalPage + 1);
+  };
+
+  const previousPage = () => {
+    setCurrentModalPage(currentModalPage - 1);
+  };
+
+  const handleLegalContractData = (signature: string, contractURI: string) => {
+    setLegalContractData({ signature, contractURI });
+  };
 
   // A generic input change handler to update the form state
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
@@ -73,10 +122,84 @@ const MattereumPassport: NextPage = () => {
   };
 
   // Form submit handler
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    // Implement form submission logic here
-    console.log(formData);
+
+    // Serialize the SVG to a string
+    const svgString = renderToStaticMarkup(
+      <BaseThumbnail
+        img={formData.image}
+        assetCategory={formData.category}
+        numberOfCertifiers={0}
+        numberOfWarranties={0}
+      />,
+    );
+
+    // Convert the string to a Buffer
+    const svgBuffer = Buffer.from(svgString);
+
+    // Create a Blob from the PNG Buffer
+    const svgBlob = new Blob([svgBuffer], { type: "image/svg+xml" });
+
+    // Create a File object from the Blob
+    const svgFile = new File([svgBlob], "nftThumbnail.svg");
+
+    // Create a FormData object
+    const pinataFormData = new FormData();
+
+    // Append the File object to the FormData object
+    pinataFormData.append("file", svgFile);
+
+    const metadata = JSON.stringify({
+      name: "Full RWA NFT Thumbnail for: " + formData.title + ", " + formData.category,
+    });
+    pinataFormData.append("pinataMetadata", metadata);
+
+    const options = JSON.stringify({
+      cidVersion: 1,
+    });
+    pinataFormData.append("pinataOptions", options);
+
+    try {
+      // open modal
+      setIsModalOpen(true);
+
+      // const nftImageURI = await pinFileToIPFS(pinataFormData);
+      // console.log(nftImageURI);
+      // setFullNFTImageURI(nftImageURI);
+    } catch (error) {
+      console.error("Error pinning to IPFS: ", error);
+    }
+  };
+
+  const handleIpfsSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // format data for submission to Pinata
+    const fullMetadata = { ...formData, legalContractData, additionalDetails: apiData ? apiData : manualFields };
+
+    const data = JSON.stringify({
+      pinataContent: {
+        ...fullMetadata,
+      },
+      pinataMetadata: {
+        name: "Full Metadata for RWA NFT " + formData.title + " " + formData.category,
+        keyvalues: {
+          date: new Date().toISOString().split("T")[0],
+        },
+      },
+      pinataOptions: {
+        cidVersion: 1,
+      },
+    });
+
+    try {
+      const metadataURI = await pinJSONToIPFS(data);
+      console.log(metadataURI);
+      setFullMetadataURI(metadataURI);
+    } catch (error) {
+      console.error("Error pinning to IPFS: ", error);
+    }
   };
 
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -93,6 +216,36 @@ const MattereumPassport: NextPage = () => {
 
       reader.readAsDataURL(uploadedFile);
     }
+  };
+
+  const mintNFT = async () => {
+    // ---------  call the contract to mint the NFT
+    // encode all collected data into a single bytes object for the smart contract
+    const abiEncoder = new ethers.AbiCoder();
+
+    const signatureBytes = ethers.getBytes(legalContractData?.signature ?? "0x00");
+
+    console.log("full URI: ", fullMetadataURI);
+    console.log("full NFT URI: ", fullNFTImageURI);
+
+    const types = ["string", "string", "string", "string", "string", "bytes", "string"];
+    const values = [
+      formData.title,
+      formData.category,
+      formData.location,
+      fullMetadataURI,
+      fullNFTImageURI,
+      signatureBytes,
+      legalContractData?.contractURI ?? "",
+    ];
+
+    const abiEncodedData = abiEncoder.encode(types, values);
+    const abiEncodedDataWithoutPrefix = abiEncodedData.substring(2);
+
+    // call the contract
+    await writeAsyncMint({
+      args: [accountState.address, BigInt(1), `0x${abiEncodedDataWithoutPrefix}`],
+    });
   };
 
   // Fetch data from the API when useApi changes
@@ -292,6 +445,117 @@ const MattereumPassport: NextPage = () => {
             )}
           </div>
         </aside>
+
+        {isModalOpen && (
+          <dialog id="mint-modal" className="modal modal-open">
+            {/* {fullNFTImageURI ? ( */}
+            <div className="modal-box">
+              {currentModalPage === 1 && (
+                <div className="form-control w-full p-4 flex flex-col justify-center items-center">
+                  <h2 className="text-2xl text-center p-4">Legal Contract</h2>
+                  <form onSubmit={nextPage} className="flex flex-col justify-around h-full">
+                    <p className="text-center">Please review the legal contract below and click the button to sign.</p>
+
+                    <LegalContract
+                      firstName={formData.firstName}
+                      lastName={formData.lastName}
+                      walletAddress={formData.walletAddress}
+                      handleLegalContractData={handleLegalContractData}
+                    />
+
+                    <FormButtons previousPage={previousPage} />
+                  </form>
+                </div>
+              )}
+
+              {currentModalPage === 2 && (
+                <div className="form-control w-full max-w-lg p-4 flex flex-col justify-center items-center">
+                  <h2 className="text-2xl text-center p-4 ml-10">Review and Submit</h2>
+                  <form className="flex flex-col justify-around" onSubmit={nextPage}>
+                    <div className="container overflow-auto h-max json-view rounded-lg p-4">
+                      <pre>
+                        <code>
+                          {JSON.stringify(
+                            { ...formData, legalContractData, additionalDetails: apiData ? apiData : manualFields },
+                            null,
+                            2,
+                          )}
+                        </code>
+                      </pre>
+                    </div>
+                    <div className="flex justify-center py-6">
+                      <button onClick={handleIpfsSubmit} className="btn btn-wide dark:btn-accent btn-primary">
+                        Submit to IPFS
+                      </button>
+                    </div>
+
+                    <FormButtons previousPage={previousPage} />
+                  </form>
+                </div>
+              )}
+
+              {currentModalPage === 3 && (
+                <>
+                  <h2 className="text-xl text-center">Successfully Uploaded!</h2>
+                  <p className="text-center">Click below to mint your RWA NFT.</p>
+                  <div className="flex justify-center p-8">
+                    <button
+                      type="button"
+                      onClick={mintNFT}
+                      className="btn btn-wide btn-lg dark:btn-accent btn-primary "
+                    >
+                      Mint NFT
+                    </button>
+                  </div>
+                </>
+              )}
+
+              <ul className="steps ml-24">
+                <li
+                  className={
+                    currentModalPage >= 1
+                      ? `step text-sm ${isDarkTheme ? "step-accent" : "step-primary"}`
+                      : "step text-sm"
+                  }
+                >
+                  <button onClick={() => setCurrentModalPage(1)} className="whitespace-normal w-20">
+                    Legal Contract
+                  </button>
+                </li>
+                <li
+                  className={
+                    currentModalPage >= 2
+                      ? `step text-sm ${isDarkTheme ? "step-accent" : "step-primary"}`
+                      : "step text-sm"
+                  }
+                >
+                  <button onClick={() => setCurrentModalPage(2)} className="whitespace-normal w-20">
+                    Submit to IPFS
+                  </button>
+                </li>
+                <li
+                  className={
+                    currentModalPage >= 3
+                      ? `step text-sm ${isDarkTheme ? "step-accent" : "step-primary"}`
+                      : "step text-sm"
+                  }
+                >
+                  <button onClick={() => setCurrentModalPage(3)} className="whitespace-normal w-20">
+                    Mint
+                  </button>
+                </li>
+              </ul>
+            </div>
+            {/* ) : (
+                <div className="flex justify-center items-center h-full">
+                  <span className="loading loading-spinner loading-lg"></span>
+                </div>
+              )} */}
+            <form method="dialog" className="modal-backdrop">
+              <button onClick={() => setIsModalOpen(false)}>close</button>
+            </form>
+          </dialog>
+        )}
       </div>
     </div>
   );
