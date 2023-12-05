7a1dfedc9f779a0e3616e7f2b41b3062d7251aaf
0xHampton <hampton.j.black@gmail.com>
2023-12-05 16:39:25 -0500

    Update proofRequest.js with new KYCCountryOfResidenceCredential

--- packages/vc-verifier-server/proofRequest.js
+++ packages/vc-verifier-server/proofRequest.js
@@ -1,19 +1,20 @@
-const { KYCAgeCredential } = require("./vcHelpers/KYCAgeCredential");
+const {
+  KYCCountryOfResidenceCredential,
+} = require("./vcHelpers/KYCCountryOfResidenceCredential");
 
 // design your own customised authentication requirement here using Query Language
 // https://0xpolygonid.github.io/tutorials/verifier/verification-library/zk-query-language/
 
-const humanReadableAuthReason = "Must be born before this year";
+const humanReadableAuthReason =
+  "Must live in a country other than listed: China, North Korea, or Russia";
 
 const credentialSubject = {
-  birthday: {
-    // users must be born before this year
-    // birthday is less than Dec 4th, 2005
-    $lt: 20051204,
+  countryCode: {
+    $nin: [156, 408, 643],
   },
 };
 
-const proofRequest = KYCAgeCredential(credentialSubject);
+const proofRequest = KYCCountryOfResidenceCredential(credentialSubject);
 
 module.exports = {
   humanReadableAuthReason,
